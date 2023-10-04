c6fa131131ceba93911b45cc31085c3c3c35ebcc
차재현 <137244332+chaajae@users.noreply.github.com>
2023-10-05 06:01:37 +0900

    Update userAddressController.java

--- Semi_Fundable/src/com/fun/member/controller/userAddressController.java
+++ Semi_Fundable/src/com/fun/member/controller/userAddressController.java
@@ -1,3 +1,11 @@
+
+
+
+// ================
+// Create by 차재현
+// ================
+
+
 package com.fun.member.controller;
 
 import java.io.IOException;
@@ -31,27 +39,7 @@ public class userAddressController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Member m = (Member) request.getSession().getAttribute("loginUser");
-		
-		
-		
-//		Member addressM = new Member(
-//						m.getUserNo(),
-//						m.getUserId(),
-//						m.getUserPwd(),
-//						m.getUserName(),
-//						m.getUserNickName(),
-//						m.getUserSsg(),
-//						m.getEmail(),
-//						m.getAddress(),
-//						m.getPhone(),
-//						m.getEnterDate(),
-//						m.getUserInterest(),
-//						m.getUserStatus()
-//						);
-//				
-				
-		//request.getSession().setAttribute("addressM", addressM); 
-		
+
 		request.getRequestDispatcher("views/member/myPageAddress.jsp").forward(request, response);
 	}
 
@@ -70,21 +58,16 @@ public class userAddressController extends HttpServlet {
 		if(Arr != null) {
 			address = String.join(",", Arr);
 		}
-		
-		
-		
+
 			addressM.setUserName(addressName);
 			addressM.setPhone(addressPhone);
 			addressM.setAddress(address);
-			
-			
+	
 			request.getSession().setAttribute("alertMsg","성공적으로 주소지를 수정했습니다.");
-			request.getSession().setAttribute("addressM", addressM); // 같은 키값으로 존재할 수 없음 => 즉 덮어쓰기 가능
+			request.getSession().setAttribute("addressM", addressM); 
 			
-			// 응답페이지 url재요청
 			request.getRequestDispatcher("views/member/myPageAddress.jsp").forward(request, response);
-	
-	
+
 	}
 
 }
